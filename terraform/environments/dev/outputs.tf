4b8edfc69b0079f6e2181471e37974e37c89e036
AmerAamir <amer.h.aamir@gmail.com>
2026-06-02 21:24:21 -0400

    Add Terraform VPC foundation

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -9,3 +9,15 @@ output "environment" {
 output "aws_region" {
   value = var.aws_region
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
